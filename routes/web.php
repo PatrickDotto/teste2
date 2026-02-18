9830434428d556e8d402ccc9b7e7f5c163ae00bf
Patrick Dotto <patrickdotto05@gmail.com>
2026-02-18 11:16:32 -0300

    Implementação do banco de dados MYSql e SQLite

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,30 @@
 <?php
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/mysql', function () {
+
+    try{
+
+        DB::connection()->getPdo();
+        echo "Conexão com a base de dados: " . DB::connection()->getDatabaseName();
+
+    } catch (\Exception $e) {
+        die("Não foi possível. Erro:" . $e->getMessage());
+    }
+    
 });
+
+Route::get('/sqlite', function(){
+    try{
+
+        DB::connection()->getPdo();
+        echo "Conexão com a base de dados: " . DB::connection()->getDatabaseName();
+
+    } catch (\Exception $e) {
+        die("Não foi possível. Erro:" . $e->getMessage());
+    }
+
+});
+''
